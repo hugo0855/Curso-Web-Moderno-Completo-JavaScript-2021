ac82cf051d4b38e9cd3be0d10659d562ca0cf403
hugo leonardo <hugoleonardo0855@gmail.com>
2021-07-27 19:12:49 -0300

    Atualizando o Curso
    Object
    Null
    Função em quase Tudo

---
+++ Fundamentos/object.js
@@ -0,0 +1,11 @@
+ const prod1 = {}
+ prod1.nome = 'Celular Ultra Mega'
+ prod1.preço = 4998.90
+prod1['Desconto Legal'] = 0.40 // Evitar atributos com espaços 
+ console.log(prod1)
+
+ const prod2 = {
+     nome:'Camisa Polo',
+     preço: 23.44
+ }
+ console.log(prod2)
